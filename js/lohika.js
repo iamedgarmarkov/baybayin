52ca397ec72167621aaadd7abdfd98b8c6718129
Mikael Romo <email@domain.com>
2014-04-17 00:22:05 +0800

    Traditional or Modern options implemented

--- js/lohika.js
+++ js/lohika.js
@@ -1,4 +1,4 @@
-function suriinAngTitik(ORIHINAL_NA_SALITA) {
+function suriinAngTitik(ORIHINAL_NA_SALITA, MOD_O_TRA) {
 	var NASURI_NA_TITIK = "";
 	var NASALIN_NA_TITIK = "";
 	var TAGATUKOY = "";
@@ -9,12 +9,24 @@ function suriinAngTitik(ORIHINAL_NA_SALITA) {
 
 			//Ginagawang space ang di makitang titik kapag ang "x" ay 1.
 			if (!(NASURI_NA_TITIK in alpabeto) && x == 1) {
-				NASURI_NA_TITIK = " ";
+				NASURI_NA_TITIK = "";
 			}
 
-			if (NASURI_NA_TITIK == " " || (NASURI_NA_TITIK in alpabeto)) {
-				NASALIN_NA_TITIK += (NASURI_NA_TITIK == " ") ? " " : alpabeto[NASURI_NA_TITIK];
-				
+			if (NASURI_NA_TITIK == " " || (NASURI_NA_TITIK in alpabeto)) {				
+
+				switch(parseInt(MOD_O_TRA)) {
+					case 1:
+						if (alpabeto[NASURI_NA_TITIK].indexOf("+") !== -1) {
+							NASALIN_NA_TITIK += " ";
+						} else {
+							NASALIN_NA_TITIK += alpabeto[NASURI_NA_TITIK];
+						}
+						break;
+					case 2:
+						NASALIN_NA_TITIK += (NASURI_NA_TITIK == " ") ? " " : alpabeto[NASURI_NA_TITIK];
+						break;
+				}
+
 				TAGATUKOY += NASURI_NA_TITIK;
 				i = ORIHINAL_NA_SALITA.indexOf(TAGATUKOY) + TAGATUKOY.length - 1;
 				break;
