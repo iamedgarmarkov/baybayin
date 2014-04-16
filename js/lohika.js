b958c88e5ee962707b830548625537e92e5c8809
Mikael Romo <email@domain.com>
2014-04-17 02:29:43 +0800

    Change logic: ignoring MAF

--- js/lohika.js
+++ js/lohika.js
@@ -7,19 +7,14 @@ function suriinAngTitik(ORIHINAL_NA_SALITA, MOD_O_TRA) {
 		for (var x = 3; x > 0; x--) {
 			NASURI_NA_TITIK = ORIHINAL_NA_SALITA.substring(i, i + x);
 
-			//Ginagawang space ang di makitang titik kapag ang "x" ay 1.
-			if (!(NASURI_NA_TITIK in alpabeto) && x == 1) {
-				NASURI_NA_TITIK = "";
-			}
-
-			if (NASURI_NA_TITIK == " " || (NASURI_NA_TITIK in alpabeto)) {				
+			if (NASURI_NA_TITIK == " " || (NASURI_NA_TITIK in alpabeto)) {
 
 				switch(parseInt(MOD_O_TRA)) {
 					case 1:
-						if (alpabeto[NASURI_NA_TITIK].indexOf("+") !== -1) {
-							NASALIN_NA_TITIK += "";
-						} else {
+						if (alpabeto[NASURI_NA_TITIK] == undefined || alpabeto[NASURI_NA_TITIK].indexOf("+") == -1) {
 							NASALIN_NA_TITIK += (NASURI_NA_TITIK == " ") ? " " : alpabeto[NASURI_NA_TITIK];
+						} else {
+							NASALIN_NA_TITIK += "";
 						}
 						break;
 					case 2:
@@ -29,6 +24,10 @@ function suriinAngTitik(ORIHINAL_NA_SALITA, MOD_O_TRA) {
 
 				TAGATUKOY += NASURI_NA_TITIK;
 				i = ORIHINAL_NA_SALITA.indexOf(TAGATUKOY) + TAGATUKOY.length - 1;
+				break;
+			} else if (!(NASURI_NA_TITIK in alpabeto) && x == 1) {
+				TAGATUKOY += NASURI_NA_TITIK;
+				i = (i == ORIHINAL_NA_SALITA.length) ? ORIHINAL_NA_SALITA.length + 1 : ORIHINAL_NA_SALITA.indexOf(TAGATUKOY) + TAGATUKOY.length - 1;
 				break;
 			}
 		};
